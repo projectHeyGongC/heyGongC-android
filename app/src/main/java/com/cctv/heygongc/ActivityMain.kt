30af4bc1c252df5adb661420fc60c3c1e42bbc55
dhjeon <sonlcr1@naver.com>
2023-12-30 23:37:38 +0900

    분석 화면 recyclerview 진행중

--- app/src/main/java/com/cctv/heygongc/ActivityMain.kt
+++ app/src/main/java/com/cctv/heygongc/ActivityMain.kt
@@ -1,5 +1,6 @@
 package com.cctv.heygongc
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.cctv.heygongc.databinding.ActivityMainBinding

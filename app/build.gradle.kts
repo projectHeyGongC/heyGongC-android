cd3accad70a12caf3d72161d11ebd3df4c6f80fd
geberani <rjqpfksl28@gmail.com>
2024-01-13 15:50:19 +0900

    targetSDK 34로 변경함

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.cctv.heygongc"
         minSdk = 24
-        targetSdk = 31
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
